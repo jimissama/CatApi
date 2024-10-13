949a14bc7f1c8c8ff2134ca6e4e97bef1e30fde0
jimissama <jimissama@gmail.com>
2024-10-13 19:36:05 +0300

    fix null argument

--- CatApi/Extensions/CatSourceResponseExtensions.cs
+++ CatApi/Extensions/CatSourceResponseExtensions.cs
@@ -8,7 +8,10 @@ public static class CatSourceResponseExtensions
 {
     public static Dictionary<string, IEnumerable<string>> GetCatTagsDictionary(this IEnumerable<CatSourceResponse> catSourceResponses, HashSet<string> existingCatIds)
     {
-        return catSourceResponses.Where(c => !existingCatIds.Contains(c.Id, new StringEqualityExtension())).ToDictionary(x => x.Id, x => x.Breeds.SelectMany(b => CatBreedTemperamentHelper.GetTagTemperamentsList([b.Temperament])).DistinctBy(t => t.ToLower()));
+        if(existingCatIds is null)
+            return new Dictionary<string, IEnumerable<string>>();
+        else
+            return catSourceResponses.Where(c => !existingCatIds.Contains(c.Id, new StringEqualityExtension())).ToDictionary(x => x.Id, x => x.Breeds.SelectMany(b => CatBreedTemperamentHelper.GetTagTemperamentsList([b.Temperament])).DistinctBy(t => t.ToLower()));
     }
 
         public static IEnumerable<TagEntity> GetTagEntities(this Dictionary<string, IEnumerable<string>> catTagsDictionary)
